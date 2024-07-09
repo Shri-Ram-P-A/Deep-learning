dc61b334d8407e4e51bea865063772f37d917bbe
Shriram P A <149064804+Shri-Ram-P-A@users.noreply.github.com>
2024-07-09 12:09:30 +0530

    upload the file

---
+++ DL2.py
@@ -0,0 +1,21 @@
+from tensorflow.keras.datasets import mnist
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+
+x_train = x_train/255
+x_test = x_test/255
+
+ANN_model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape = (28,28)),
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dense(64,activation = 'relu'),
+    tf.keras.layers.Dense(10,activation = 'softmax')]
+)
+
+ANN_model.compile(loss="sparse_categorical_crossentropy",optimizer = 'adam',metrics = ['accuracy'])
+
+ANN_model.fit(x_train,y_train,epochs = 100)
+t = ANN_model.evaluate(x_test,y_test)
+print("Accuracy :",t[1])
